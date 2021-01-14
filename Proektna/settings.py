5b2a03d1ab467c2b2c0104ae9e34ddfca3ac1ff3
Nenad <ristovn18@gmail.com>
2021-01-14 13:12:19 +0100

    cors-headers

--- Proektna/settings.py
+++ Proektna/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Proektna.urls'
@@ -125,4 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+
+CORS_ALLOW_ALL_ORIGINS = True
 
